6c51e89a16a9dba5c17a9cad2095705d1b727105
Shamilok <shmi1din@gmail.com>
2023-07-09 17:53:47 +0500

    settings: currency, price; home: basic funсtionality

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,103 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, Modal, TextInput, FlatList } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Home() {
+const Home = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [deviceName, setDeviceName] = useState('');
+  const [deviceWatts, setDeviceWatts] = useState('');
+  const [deviceQuantity, setDeviceQuantity] = useState('');
+  const [devices, setDevices] = useState([]);
+
+  const [currency, setCurrency] = useState("");
+  const [price, setPrice] = useState("");
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const savedCurrency = await AsyncStorage.getItem("currency");
+        const savedPrice = await AsyncStorage.getItem("price");
+
+      if (savedCurrency) {
+        setCurrency(savedCurrency);
+      }
+      if (savedPrice) {
+        setPrice(savedPrice);
+      }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+  loadData();
+
+  }, []);
+
+  const addDevice = () => {
+    if (deviceName && deviceWatts && deviceQuantity) {
+      const newDevice = {
+        name: deviceName,
+        watts: deviceWatts,
+        quantity: deviceQuantity,
+      };
+
+      setDevices([...devices, newDevice]);
+      setModalVisible(false);
+      setDeviceName('');
+      setDeviceWatts('');
+      setDeviceQuantity('');
+    }
+  };
+
+  const getTotalWatts = () => {
+    return devices.reduce((total, device) => total + (device.watts * device.quantity), 0);
+  };
+
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Home</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Добавить электроустройство" onPress={() => setModalVisible(true)} />
+      <Text>Список устройств:</Text>
+      <Modal visible={modalVisible}>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <TextInput
+            placeholder="Имя электроустройства"
+            value={deviceName}
+            onChangeText={text => setDeviceName(text)}
+          />
+          <TextInput
+            placeholder="Ватты"
+            value={deviceWatts}
+            onChangeText={text => setDeviceWatts(text)}
+            keyboardType="numeric"
+          />
+          <TextInput
+            placeholder="Количество"
+            value={deviceQuantity}
+            onChangeText={text => setDeviceQuantity(text)}
+            keyboardType="numeric"
+          />
+
+          <Button title="Добавить" onPress={addDevice} />
+          <Button title="Отмена" onPress={() => setModalVisible(false)} />
+        </View>
+      </Modal>
+
+      <FlatList
+        data={devices}
+        renderItem={({ item }) => (
+          <View style={{ flexDirection: 'row' }}>
+            <Text>{item.name}, {item.watts}ватт, {item.quantity}шт</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
+      <Text>Общее количество ватт: {getTotalWatts()}</Text>
+      <Text>Общее количество Кватт: {getTotalWatts() / 1000}</Text>
+      <Text>Цена электричества за день: {getTotalWatts() / 1000 * 24 * price} </Text>
+      <Text>Цена электричества за месяц: {(getTotalWatts() / 1000 * 24 * price) * 30}</Text>
     </View>
-  )
-}
+  );
+};
+
+export default Home;
--- screens/Settings.js
+++ screens/Settings.js
@@ -1,10 +1,111 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Settings() {
-  return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <Text>Settings</Text>
-    </View>
-  )
+  const [currency, setCurrency] = useState("");
+  const [price, setPrice] = useState("");
+
+useEffect(() => {
+  const loadData = async () => {
+    try {
+      const savedCurrency = await AsyncStorage.getItem("currency");
+      const savedPrice = await AsyncStorage.getItem("price");
+
+    if (savedCurrency) {
+      setCurrency(savedCurrency);
+    }
+    if (savedPrice) {
+      setPrice(savedPrice);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+loadData();
+
+}, []);
+
+const handleCurrencyChange = (value) => {
+    setCurrency(value);
+    AsyncStorage.setItem("currency", value);
+  };
+
+const handlePriceChange = (text) => {
+    setPrice(text);
+    AsyncStorage.setItem("price", text);
+  };
+
+return (
+    <View style={styles.container}>
+      <View style={styles.formItem}>
+        <Text style={styles.label}>Select currency:</Text>
+        <View style={styles.picker}>
+          <Picker
+            
+            selectedValue={currency}
+            onValueChange={handleCurrencyChange}>
+            <Picker.Item label="EUR" value="EUR" />
+            <Picker.Item label="USD" value="USD" />
+            <Picker.Item label="RUB" value="RUB" />
+          </Picker>
+        </View>
+        
+      </View>
+      <View style={styles.formItem}>
+        <Text style={styles.label}>Enter the price per kWh:</Text>
+        <TextInput
+          style={styles.input}
+          value={price}
+          onChangeText={handlePriceChange}
+          keyboardType="numeric"
+        />
+      </View>
+      <View style={styles.result}>
+        <Text style={styles.text}>Currency: {currency}</Text>
+        <Text style={styles.text}>Price per kWh: {price}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  formItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginRight: 10,
+  },
+  picker: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    // paddingVertical: 5,
+    // paddingHorizontal: 10,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  result: {
+    marginTop: 20,
+  },
+  text: {
+    fontSize: 16,
+  },
+});
